89da62c9c5b81f67f00b0ecd1505c195587fbba5
Toan Toan <67469007+mytoandeptrai@users.noreply.github.com>
2023-09-09 20:01:50 +0700

    Update Feature upload single image

--- src/modules/upload/upload.controller.ts
+++ src/modules/upload/upload.controller.ts
@@ -2,11 +2,12 @@ import {
   Body,
   Controller,
   Post,
+  UploadedFile,
   UploadedFiles,
   UseGuards,
   UseInterceptors,
 } from '@nestjs/common';
-import { FilesInterceptor } from '@nestjs/platform-express';
+import { FileInterceptor, FilesInterceptor } from '@nestjs/platform-express';
 import { ApiBearerAuth, ApiConsumes, ApiTags } from '@nestjs/swagger';
 import { MyTokenAuthGuard } from 'common';
 import { UploadTool } from 'tools';
@@ -26,10 +27,22 @@ export class UploadController {
   @UseInterceptors(
     FilesInterceptor('files', MAXIMUM_COUNT_FILES, UploadTool.imageUpload),
   )
-  uploadMedia(
+  uploadMedias(
     @UploadedFiles() files: Express.Multer.File[],
     @Body() meta: UploadMetaInput,
   ): Promise<string[]> {
     return this.uploadService.uploadMedias(files, meta);
+  }
+
+  @Post('/image')
+  @ApiBearerAuth()
+  @UseGuards(MyTokenAuthGuard)
+  @ApiConsumes('multipart/form-data')
+  @UseInterceptors(FileInterceptor('image', UploadTool.imageUpload))
+  uploadMedia(
+    @UploadedFile() image: Express.Multer.File,
+    @Body() meta: UploadMetaInput,
+  ) {
+    return this.uploadService.uploadMedias([image], meta);
   }
 }
